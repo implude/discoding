1ebf6acbc9b05fc8a1a29c4c129bb1b495e835f5
redstonecube <superhoy0509@gmail.com>
2021-10-06 21:56:10 +0900

    retrofit add, waiting DB

--- app/src/main/java/com/example/discoding/Appmain.kt
+++ app/src/main/java/com/example/discoding/Appmain.kt
@@ -3,11 +3,59 @@ package com.example.discoding
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class Appmain : AppCompatActivity() {
+
+    var gson= GsonBuilder().setLenient().create()
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://5ca3-121-66-18-107.ngrok.io/")
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+
+    private val service = retrofit.create(UserRequest ::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+
+        service.getuserinfo("online","a","a","a").enqueue(object : Callback<MemberResult> {
+            override fun onResponse(
+                call: Call<MemberResult>,
+                response: Response<MemberResult>
+            ) {
+                Log.d("body",response.body()?.val1.toString())
+                if(/*DB에 저장된 토큰이 있다면*/)
+                {
+                    service.getuserinfo("login",/*토큰 여기에*/,"a","a").enqueue(object : Callback<MemberResult> {
+                        override fun onResponse(
+                            call: Call<MemberResult>,
+                            response: Response<MemberResult>
+                        ) {
+                            //어케어케 해봐
+                            Log.d("body",response.body()?.val1.toString())
+                        }
+
+                        override fun onFailure(call: Call<MemberResult>, t: Throwable) {
+                            Log.d("result",t.toString())
+                        }
+
+
+                    })
+                }
+            }
+
+            override fun onFailure(call: Call<MemberResult>, t: Throwable) {
+                Log.d("result",t.toString())
+            }
+        })
+
 
         val mHosting_btn = findViewById<android.widget.Button>(R.id.mHosting_btn) //메인페이지에 있는 호스팅 버튼
         val mShare_btn = findViewById<android.widget.Button>(R.id.mShare_btn) //메인페이지에 있는 공유 버튼
@@ -17,10 +65,12 @@ class Appmain : AppCompatActivity() {
             val mGo_hosting = Intent(this, HostingActivity::class.java)
             startActivity(mGo_hosting)
         }
+
         mShare_btn.setOnClickListener() { //메인에서 공유 페이지로
             val mGo_share = Intent(this, Share::class.java)
             startActivity(mGo_share)
         }
+
         mPlus_btn.setOnClickListener() { //메인에서 봇생성페이지로
             val mGo_CreateBot = Intent(this, CreateBot::class.java)
             startActivity(mGo_CreateBot)
---
+++ app/src/main/java/com/example/discoding/request.kt
@@ -0,0 +1,30 @@
+package com.example.discoding
+
+
+import com.google.gson.GsonBuilder
+import com.google.gson.annotations.SerializedName
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.*
+
+data class MemberResult(
+    val val1: String,
+    val val2: String,
+    val val3: String
+)
+
+interface UserRequest {
+
+    @FormUrlEncoded
+    @POST("/{path}")
+    fun getuserinfo(
+        @Path("path") path: String,
+        @Field("userId") userId: String,
+        @Field("botName") botname: String,
+        @Field("eventName") eventname: String
+    ): Call<MemberResult>
+
+}
